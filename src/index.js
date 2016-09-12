6c5693158284762e6eafb6cd273ef06f6f765a70
migutw42 <ubuntu@dfc2eabdf236>
2016-09-12 06:31:40 +0000

    change font

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 import './stylesheets/index.css';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import theme from './theme';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 import App from './components/App';
-import reducer from './reducers/reducer'
+import reducer from './reducers/reducer';
 
 const devToolsExtension = window.devToolsExtension && window.devToolsExtension();
 
@@ -24,7 +24,7 @@ const store = createStoreWithMiddleware(reducer, devToolsExtension);
 
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={theme}>
       <App />
     </MuiThemeProvider>
   </Provider>,
