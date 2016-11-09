d422ca5fd4de0d0c5ca79d46dffded6b536a9ac4
migutw42 <ez.do.subi@gmail.com>
2016-11-09 12:45:38 +0000

    adapt responsive

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 import NovelList from './NovelList';
 import SearchBar from './SearchBar';
+import { flexContainer } from '../styles/style'
 
 export default function App () {
+  const content = {
+    maxWidth: 750
+  };
+
+  const side = {
+    flex: 1
+  };
+
   return (
     <div>
-      <SearchBar/>
-      <NovelList/>
+      <header>
+        <SearchBar/>
+      </header>
+      <div style={flexContainer}>
+        <div style={side}></div>
+        <div style={content}>
+          <NovelList/>
+        </div>
+        <div style={side}></div>
+      </div>
     </div>
   );
 };
