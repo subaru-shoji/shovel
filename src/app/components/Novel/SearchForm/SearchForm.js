1d0791a8c404ff7b7670bc307028cd4de5339032
東海林　昴 <s-shoji@bnba.jp>
2016-11-30 18:24:26 +0900

    Implement CommonBar

--- src/app/components/Novel/SearchForm/SearchForm.js
+++ src/app/components/Novel/SearchForm/SearchForm.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
-import AppBar from 'material-ui/AppBar';
+import SearchField from './SearchField';
+import SearchButton from './SearchButton';
+import SearchToolBar from './SearchToolBar';
 
-import SearchField from './SearchField'
-import SearchButton from './SearchButton'
-import SearchToolBar from './SearchToolBar'
+import CommonBar from '../../Common/CommonBar'
 
-import { SHOW_PER_SEARCH } from '../../../../constants';
+import {SHOW_PER_SEARCH} from '../../../../constants';
 
-
-export default SearchForm = ({searchMethod, openMenu}) => {
+export default SearchForm = ({searchMethod}) => {
   return (
     <form onSubmit={searchMethod}>
-      <AppBar
+      <CommonBar
         title={(<SearchField/>)}
-        onLeftIconButtonTouchTap={openMenu}
-        iconElementRight={(<SearchButton searchMethod={searchMethod}/>)}
-      />
-      <input name="lim" type="hidden" value={SHOW_PER_SEARCH} />
+        iconElementRight={(<SearchButton searchMethod={searchMethod}/>)}/>
+      <input name="lim" type="hidden" value={SHOW_PER_SEARCH}/>
       <div>
         <SearchToolBar/>
       </div>
