29c4a925251e2d05acaee40a607a8bad71bf8455
migutw42 <ez.do.subi@gmail.com>
2016-12-02 06:27:04 +0000

    Fix to test redux connected component.

--- src/app/components/Common/CommonBar/CommonBar.test.js
+++ src/app/components/Common/CommonBar/CommonBar.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { createMockStore } from 'redux-test-utils';
 import CommonBar from './CommonBar';
 
 it('renders without crashing', () => {
-  shallow(<CommonBar />);
+  const context = {store: createMockStore('state')};
+  shallow(<CommonBar />, {context});
 });
