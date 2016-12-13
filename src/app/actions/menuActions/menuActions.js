b5b9b968d612c44e15f5a4c82329f64f2b8fd1a7
migutw42 <ez.do.subi@gmail.com>
2016-12-13 03:07:12 +0000

    Add actions.

--- src/app/actions/menuActions/menuActions.js
+++ src/app/actions/menuActions/menuActions.js
@@ -1,13 +1,23 @@
 export const openMenu = () => {
   return {
-    type: 'OPEN_MENU',
-    payload: true
+    type: 'OPEN_MENU'
   }
 };
 
 export const closeMenu = () => {
   return {
-    type: 'CLOSE_MENU',
-    payload: true
+    type: 'CLOSE_MENU'
+  }
+};
+
+export const showMenuDocked = () => {
+  return {
+    type: 'SHOW_MENU_DOCKED'
+  }
+};
+
+export const showMenuUndocked = () => {
+  return {
+    type: 'SHOW_MENU_UNDOCKED'
   }
 };
--- src/app/actions/menuActions/menuActions.test.js
+++ src/app/actions/menuActions/menuActions.test.js
@@ -1,14 +1,18 @@
 import { expect } from 'chai';
 
-import { openMenu, closeMenu } from './menuActions';
+import { openMenu, closeMenu, showMenuDocked, showMenuUndocked } from './menuActions';
 
 it('return valid action', () => {
   expect(openMenu()).to.eql({
-    type: 'OPEN_MENU',
-    payload: true
+    type: 'OPEN_MENU'
   });
   expect(closeMenu()).to.eql({
-    type: 'CLOSE_MENU',
-    payload: true
+    type: 'CLOSE_MENU'
+  });
+  expect(showMenuDocked()).to.eql({
+    type: 'SHOW_MENU_DOCKED'
+  });
+  expect(showMenuUndocked()).to.eql({
+    type: 'SHOW_MENU_UNDOCKED'
   });
 });
