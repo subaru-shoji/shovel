ed77fc52d9681edf6837b5fd0c91f14341e67943
東海林　昴 <s-shoji@bnba.jp>
2017-01-24 17:52:32 +0900

    Move drop donws

---
+++ src/app/pages/novels/components/layouts/DropDownField/DropDownField.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+import { grey400, transparent} from 'material-ui/styles/colors';
+
+
+export default class DropDownField extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value: Object.keys(this.props.hashMap)[0]};
+  }
+  handleChange(event, index, value){
+    this.setState({value: value});
+  }
+  render(){
+    const labelStyle = {
+      fontSize: 12
+    };
+
+    const iconStyle = {
+      fill: grey400
+    };
+
+    const underlineStyle = {
+      borderColor: transparent
+    };
+
+
+    const hashMap = this.props.hashMap;
+    const menus = Object.keys(hashMap).map((key)=>{
+      return (
+        <MenuItem key={key} value={key} primaryText={hashMap[key]}/>
+      );
+    })
+
+    return (
+      <span>
+        <input type='hidden' name={this.props.name} value={this.state.value}/>
+        <DropDownMenu
+          autoWidth={true}
+          value={this.state.value}
+          onChange={this.handleChange.bind(this)}
+          {...{iconStyle, underlineStyle, labelStyle}}
+        >
+          {menus}
+        </DropDownMenu>
+      </span>
+    );
+  }
+}
---
+++ src/app/pages/novels/components/layouts/DropDownField/DropDownField.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import DropDownField from './DropDownField';
+
+it('renders without crashing', () => {
+  shallow(<DropDownField hashMap={{test: 'test'}} />);
+});
